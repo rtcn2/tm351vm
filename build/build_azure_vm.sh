529b5e7619277f80ec50aed4e4a8eda9412cac32
Rod Norfor <rod.norfor@open.ac.uk>
2019-09-18 08:15:03 +0000

    added script to automate build as part of azure vm build

---
+++ build/build_azure_vm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone https://github.com/innovationOUtside/tm351vm --depth 1 --branch=master ~/tm351vm
+cd ./tm351vm/build/
+
+token='myImpossiblyHardToGuessToken'
+
+#add NotebookApp token
+# jupyter-base/services/jupyter.service
+#--NotebookApp.token=myImpossiblyHardToGuessToken
+sed -i -E "s/--NotebookApp\.token=/--NotebookApp.token=$token/" jupyter-base/services/jupyter.service
+
+#stop build of mongo sharded demo
+#monolithic_build.sh
+# source $BUILDDIR/mong/sharded/mongo_cluster.sh
+sed -i "s/source \$BUILDDIR\/mongo\/sharded\/mongo_cluster\.sh/# source \$BUILDDIR\/mongo\/sharded\/mongo_cluster.sh/" monolithic_build.sh
+
+sudo ./monolithic_build.sh
+
+#output urls for services
+
